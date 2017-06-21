e04a02bc98ca975e78d397597196507156767b73
Ian Sanders <iansan5653@gmail.com>
2017-06-21 17:18:26 -0400

    Python 3.4

--- app.py
+++ app.py
@@ -19,43 +19,43 @@ client = discord.Client()
 
 
 @client.event
-async def on_ready():
+@asyncio.coroutine on_ready():
     print('Logged in')
 
 # SCPBotCode
 
 
 @client.event
-async def on_message(message):
+@asyncio.coroutine on_message(message):
     trigMessage = message.content.lower()
 # ScpBotCode
     SCPCodes = [t for t in trigMessage.split() if t.startswith('scp-') or t.startswith('SCP-') or t.startswith('Scp-')]
     if SCPCodes:
-        await client.send_typing(message.channel)
+        yield from client.send_typing(message.channel)
         msg = SCPCodes[0].split('-', 1)[1]
 
         if len(msg) <= 4 and len(msg) >= 3 and msg.isdigit():
-            tmp_msg = await client.send_message(message.channel, '**Link:** http://www.scp-wiki.net/scp-' + msg + ' *Checking for existence...*')
+            tmp_msg = yield from client.send_message(message.channel, '**Link:** http://www.scp-wiki.net/scp-' + msg + ' *Checking for existence...*')
             scp = requests.get('http://www.scp-wiki.net/scp-' + msg)
             if scp.status_code == 200:
-                # await client.send_typing(message.channel)
-                await client.edit_message(tmp_msg, '**Link:** http://www.scp-wiki.net/scp-' + msg + ' *SCP exists!*')
+                # yield from client.send_typing(message.channel)
+                yield from client.edit_message(tmp_msg, '**Link:** http://www.scp-wiki.net/scp-' + msg + ' *SCP exists!*')
 
                 # Someday images may get sent. Not today.
                 # tree = BeautifulSoup(scp.text, "lxml")
                 # img_link = tree.find_all('div', class_="scp-image-block")[0].img.get('src')
-                # await client.send_file(message.channel, img_link, filename='SCP-' + msg + '_img', content=None, tts=False)
+                # yield from client.send_file(message.channel, img_link, filename='SCP-' + msg + '_img', content=None, tts=False)
 
             elif scp.status_code == 404:
-                await client.edit_message(tmp_msg, '~~**Link:** http://www.scp-wiki.net/scp-' + msg + '~~ *SCP does not exist.*')
+                yield from client.edit_message(tmp_msg, '~~**Link:** http://www.scp-wiki.net/scp-' + msg + '~~ *SCP does not exist.*')
             else:
-                await client.edit_message(tmp_msg, '**Link:** http://www.scp-wiki.net/scp-' + msg + ' *Unable to determine if this SCP exists.*')
+                yield from client.edit_message(tmp_msg, '**Link:** http://www.scp-wiki.net/scp-' + msg + ' *Unable to determine if this SCP exists.*')
 
         else:
-            await client.send_message(message.channel, 'SCP must be a 3 or 4 digit number. Example: `SCP-1175`')
+            yield from client.send_message(message.channel, 'SCP must be a 3 or 4 digit number. Example: `SCP-1175`')
 # AyyLmaoBotCode
     elif trigMessage.startswith(trigPref + 'ayy'):
-        await client.send_message(message.channel, 'lmao')
+        yield from client.send_message(message.channel, 'lmao')
 # StarWarsTitleBotCode
     elif trigMessage.startswith(trigPref + 'sw'):
         msg = message.content.split(' ', 1)[1]
@@ -71,37 +71,37 @@ async def on_message(message):
                 8: 'The Last Jedi',
                 9: '[TBA]'
             }
-            await client.send_message(message.channel, 'That Star Wars movie is called *' + titles[int(msg)] + '*.')
+            yield from client.send_message(message.channel, 'That Star Wars movie is called *' + titles[int(msg)] + '*.')
 # LoadingBarBotCode
     elif trigMessage.startswith(trigPref + '!load'):
         x = random.randrange(0, 10)
-        loading = await client.send_message(message.channel, '`0%   -------------------`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`10%  ||------------------`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`20%  ||||----------------`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`30%  ||||||--------------`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`40%  ||||||||------------`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`50%  ||||||||||----------`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`60%  ||||||||||||--------`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`70%  ||||||||||||||------`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`80%  ||||||||||||||||----`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`90%  ||||||||||||||||||--`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`95%  |||||||||||||||||||-`')
-        await asyncio.sleep(x)
-        await client.edit_message(loading, '`100% ||||||||||||||||||||`')
-        await client.send_message(message.channel, 'Loading Complete!')
+        loading = yield from client.send_message(message.channel, '`0%   -------------------`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`10%  ||------------------`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`20%  ||||----------------`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`30%  ||||||--------------`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`40%  ||||||||------------`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`50%  ||||||||||----------`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`60%  ||||||||||||--------`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`70%  ||||||||||||||------`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`80%  ||||||||||||||||----`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`90%  ||||||||||||||||||--`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`95%  |||||||||||||||||||-`')
+        yield from asyncio.sleep(x)
+        yield from client.edit_message(loading, '`100% ||||||||||||||||||||`')
+        yield from client.send_message(message.channel, 'Loading Complete!')
 # PizzaOrderBotCode
     elif trigMessage.startswith(trigPref + '`sudo order pizza`'):
-        await client.send_message(message.channel, ':pizza:')
+        yield from client.send_message(message.channel, ':pizza:')
 # RandomNumberGenCode
     elif trigMessage.startswith(trigPref + '!rand'):
         msgsplit = message.content.split(' ', 3)
@@ -110,50 +110,50 @@ async def on_message(message):
 
         if minval.isdigit() and maxval.isdigit():
             value = random.randrange(int(minval), int(maxval))
-            await client.send_message(message.channel, 'Your number is: **' + str(value) + '**.')
+            yield from client.send_message(message.channel, 'Your number is: **' + str(value) + '**.')
         else:
-            await client.send_message(message.channel, 'Invalid command, use syntax `!rand minimum maximum`.')
+            yield from client.send_message(message.channel, 'Invalid command, use syntax `!rand minimum maximum`.')
 # RedditBotCode
     elif trigMessage.startswith(trigPref + 'r '):
         subreddit = message.content.split(' ', 1)[1]
-        await client.send_message(message.channel, 'https://www.reddit.com/r/' + subreddit)
+        yield from client.send_message(message.channel, 'https://www.reddit.com/r/' + subreddit)
 
     elif trigMessage.startswith(trigPref + 'u '):
         user = message.content.split(' ', 1)[1]
-        await client.send_message(message.channel, 'https://www.reddit.com/u/' + user)
+        yield from client.send_message(message.channel, 'https://www.reddit.com/u/' + user)
         r_user = reddit.redditor(user)
         print(r_user.submissions.new())
-        await client.send_message(message.channel, 'Link Karma: **' + str(r_user.link_karma) + '**')
-        await client.send_message(message.channel, 'Comment Karma: **' + str(r_user.comment_karma) + '**')
+        yield from client.send_message(message.channel, 'Link Karma: **' + str(r_user.link_karma) + '**')
+        yield from client.send_message(message.channel, 'Comment Karma: **' + str(r_user.comment_karma) + '**')
 # RandomIntegerSpam
     elif trigMessage.startswith(trigPref + '!numberspam'):
         loop = 1
         while loop == 1:
             y = random.randrange(1, 100)
-            await client.send_message(message.channel, y)
+            yield from client.send_message(message.channel, y)
 # PasswordGen
     elif trigMessage.startswith(trigPref + '!p'):
         passsplit = message.content.split(' ', 1)
         passlen = int(passsplit[1])
         randtextstr = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(passlen))
-        await client.send_message(message.channel, 'Your password is: ' + randtextstr)
+        yield from client.send_message(message.channel, 'Your password is: ' + randtextstr)
 # LennyCode
     elif trigMessage.startswith(trigPref + 'lenny'):
-        await client.send_message(message.channel, '( ͡° ͜ʖ ͡°)')
+        yield from client.send_message(message.channel, '( ͡° ͜ʖ ͡°)')
 # DiceRoll
     elif trigMessage.startswith(trigPref + '!roll'):
             dieRoll = random.randrange(1, 20)
-            await client.send_message(message.channel, dieRoll)
+            yield from client.send_message(message.channel, dieRoll)
 # WikipediaSearch
     elif trigMessage.startswith(trigPref + 'wiki'):
         wikiSplit = message.content.split(' ')
         wikiSplit.pop(0)
         wikiLink = 'https://en.wikipedia.org/wiki/'
         wiki = "_".join(wikiSplit)
-        await client.send_message(message.channel, wikiLink + wiki)
+        yield from client.send_message(message.channel, wikiLink + wiki)
 # Magic8Ball
     elif trigMessage.startswith(trigPref + 'magic8ball'):
-        await client.send_message(message.channel, 'To use the Magic 8 Ball, type !8ball and your question...')
+        yield from client.send_message(message.channel, 'To use the Magic 8 Ball, type !8ball and your question...')
     elif trigMessage.startswith(trigPref + '!8ball'):
         answers = ['It is certain',
                    'It is decidedly so',
@@ -177,16 +177,16 @@ async def on_message(message):
                    'I am very doubtful',
                    "Ha, don't kid yourself"]
         chosenans = random.choice(answers)
-        await client.send_message(message.channel, chosenans)
+        yield from client.send_message(message.channel, chosenans)
 # RandomIntegerSpam
     elif trigMessage.startswith(trigPref + '!test'):
         loop = 1
         while loop == 1:
-            await client.send_message(message.channel, 'PLAYERUNKNOWNS BATTLEGROUNDS')
+            yield from client.send_message(message.channel, 'PLAYERUNKNOWNS BATTLEGROUNDS')
 # Error
     elif trigMessage.startswith(trigPref + 'help'):
-        await client.send_message(message.channel, 'ERROR CANNOT PROCESS FUNCTION REPORT TO AN ADMINISTRATOR IMMEDIATELY')
+        yield from client.send_message(message.channel, 'ERROR CANNOT PROCESS FUNCTION REPORT TO AN ADMINISTRATOR IMMEDIATELY')
 # No problem
     elif trigMessage == 'thank you':
-        await client.send_message(message.channel, 'You\'re welcome.')
+        yield from client.send_message(message.channel, 'You\'re welcome.')
 client.run(token)
